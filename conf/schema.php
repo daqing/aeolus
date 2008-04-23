94696fd240ea486a7ca97b6ec375b057e66502f3
Qingcheng Zhang <kinch.zhang@gmail.com>
2008-04-23 20:34:54 +0800

    Added language files

--- conf/schema.php
+++
@@ -1,8 +0,0 @@
-<?php  if( basename($_SERVER['REQUEST_URI']) == basename(__FILE__) ){
-          die('Your request is invalid.<br/>System exited.');
-	   }
-    /**
-	 * Database schema 
-	 *
-	 */
-?>
