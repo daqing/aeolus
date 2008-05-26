01a7f12296e687f758a7c5b116c2ecf6e7a213b2
Kinch Zhang <kinch.zhang@gmail.com>
2008-05-26 17:17:42 +0800

    Aeolus 0.2.1

---
+++ app/benchmark/view/IndexIndexView.php
@@ -0,0 +1,28 @@
+<?php if(! defined('APP_STARTED')){die('<h3>BAD REQUEST.</h3>');}
+     
+  /**
+   * IndexIndexView view class in 'benchmark' module
+   *
+   */
+
+  class IndexIndexView extends AeolusView
+  {
+    public function showSidebar()
+    {
+	  echo 'Benchmark Home';
+    }
+
+    public function showContent()
+    {
+	  echo '<div class="section">';
+	  echo 'Welcome to the benchmark page';
+	  echo '</div>';
+    }
+
+    public function showScript()
+    {
+      ?>
+      <?php
+    }
+  }
+?>
