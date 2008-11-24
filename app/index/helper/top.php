5643009ebf2cfafbcb6ad3ce62365dcf7affce27
Qingcheng Zhang <daqing@bogon.(none)>
2008-11-24 14:50:49 +0800

    make Logo refer to the home url

--- app/index/helper/top.php
+++ app/index/helper/top.php
@@ -3,6 +3,6 @@
   /* top helper in index group */
   function top()
   {
-    echo '<img src="' . SUB_URL .'/pub/image/logo.gif" width="165" height="34" />';
+    echo '<a href="' . OP_BASE . '" alt="home"><img src="' . SUB_URL .'/pub/image/logo.gif" width="165" height="34" /></a>';
   }
 ?>
