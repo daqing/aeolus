247d2da4abd92d3263ac67c30eb6686f23c92b65
Qingcheng Zhang <daqing@bogon.(none)>
2008-12-11 18:57:27 +0800

    Add an exception class to Aeolus

--- index.php
+++ index.php
@@ -1,25 +1,37 @@
 <?php
+  
+    /*
+     * This is the frontend for Aeolus system.
+     *
+     */
+  
+    define('A_PREFIX', dirname(__FILE__).'/');
+  
+    // Load application configuration
+    require 'etc/app.php';
 
-  /*
-   * This is the frontend for Aeolus system.
-   *
-   */
+    if (!APP_ENABLED) {
+      require 'pub/error/aeolus_na.html';
+      die();
+    }
+  
+    // Bootstrap
+    require 'sys/bootstrap.php';
+  
+    // Load front controller
+    require 'kernel/AeoFront.php';
+  
+  
+    // Load Aeolus exception class
+    require 'sys/AeoException.php';
+  
+    try {
+        $front = new AeoFront();
+  
+        $front->run();
+    } catch (AeoException $e) {
+        // TODO: display user-friendly error page
+        die($e->getMessage());
+    }
 
-  define('A_PREFIX', dirname(__FILE__).'/');
-
-  require 'etc/app.php';
-  if (!APP_ENABLED) {
-    require 'pub/error/aeolus_na.html';
-    die();
-  }
-
-  // Bootstrap
-  require 'sys/bootstrap.php';
-
-  // Load front controller
-  require 'kernel/AeoFront.php';
-
-  $front = new AeoFront();
-
-  $front->run();
 ?>
---
+++ sys/AeoException.php
@@ -0,0 +1,21 @@
+<?php
+    
+    /*
+     * Aeolus exception class
+     *
+     */
+    
+    class AeoException extends Exception
+    {
+        public function __construct($message, $code=0)
+        {
+            parent::__construct($message, $code);
+        }
+  
+        public function __toString()
+        {
+            return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        }
+  
+    }
+?>
