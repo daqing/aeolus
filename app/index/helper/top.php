4ab9633b33c24788f1361c6ee6d6a5e287a4811a
Kinch Zhang <kinch.zhang@gmail.com>
2009-02-22 20:56:50 +0800

    delete the heading slash of the logo address

--- app/index/helper/top.php
+++ app/index/helper/top.php
@@ -3,6 +3,6 @@
   /* top helper in index group */
   function top()
   {
-    echo '<a href="' . OP_BASE . '" alt="home"><img src="' . SUB_URL .'/public/image/logo.gif" width="165" height="34" /></a>';
+    echo '<a href="' . OP_BASE . '" alt="home"><img src="' . SUB_URL .'public/image/logo.gif" width="165" height="34" /></a>';
   }
 ?>
