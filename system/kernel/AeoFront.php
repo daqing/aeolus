33017d4979a98d14f43e327309869354a2059391
Kinch Zhang <kinch.zhang@gmail.com>
2009-03-08 15:48:25 +0800

    colorize error message

--- system/kernel/AeoFront.php
+++ system/kernel/AeoFront.php
@@ -1,49 +1,50 @@
 <?php
+
   /*
    * Front controller
    */
 
   class AeoFront
   {
-	private $request;
+    private $request;
 
-	private $result;
+    private $result;
 
-	function __construct()
-	{
-	  $this->request = strtolower($_SERVER['REQUEST_URI']);
+    function __construct()
+    {
+      $this->request = strtolower($_SERVER['REQUEST_URI']);
 
-	  $this->result['module'] = 'index';
-	  $this->result['controller'] = 'index_index';
-	  $this->result['argv'] = array();
-	}
+      $this->result['module'] = 'index';
+      $this->result['controller'] = 'index_index';
+      $this->result['argv'] = array();
+    }
 
-	public function run()
-	{
-	  $this->process();
-	  $this->launch();
-	}
+    public function run()
+    {
+      $this->process();
+      $this->launch();
+    }
 
-	/* Process the HTTP request */
-	private function process()
-	{
-	  /* Remove output base url from the request */
-	  $this->request = substr($this->request, strlen(BASE_URL));
-	  $this->request = trim($this->request, '/\\');
+    /* Process the HTTP request */
+    private function process()
+    {
+      /* Remove output base url from the request */
+      $this->request = substr($this->request, strlen(BASE_URL));
+      $this->request = trim($this->request, '/\\');
 
-	  if (strpos($this->request, '(') || strpos($this->request, '%'))
-		return;
+      if (strpos($this->request, '(') || strpos($this->request, '%'))
+        return;
 
       /* Get segments */
-	  $seg = (strlen($this->request)) ? explode('/', $this->request) : '/';
+      $seg = (strlen($this->request)) ? explode('/', $this->request) : '/';
 
-	  if ('/' !== $seg && is_array($seg)) {
-	    require A_PREFIX.'config/system/module.php';
+      if ('/' !== $seg && is_array($seg)) {
+        require A_PREFIX.'config/system/module.php';
 
-		$size = count($seg);
-		switch ($size) {
-		case 1:
-		  if (in_array($seg[0], $module)) {
+        $size = count($seg);
+        switch ($size) {
+        case 1:
+          if (in_array($seg[0], $module)) {
               // default controller in this module
               $this->result['module'] = $seg[0];
               $this->result['controller'] = $seg[0] . '_index';
@@ -51,79 +52,79 @@
               // this controller in 'index' module
               $this->result['controller'] = 'index_' . $seg[0];
           }
-		break;
-		case 2:
-		  if (in_array($seg[0], $module)) {
+        break;
+        case 2:
+          if (in_array($seg[0], $module)) {
               $this->result['module'] = $seg[0];
               $this->result['controller'] = $seg[0] . '_' .$seg[1];
-		  } else {
+          } else {
               // this controller in 'index' group with argv
-		      $this->result['controller'] = 'index_' . $seg[0];
-		      $this->result['argv'][] = $seg[1];
-		  }
-		break;
-		default:
-		  if (in_array($seg[0], $module)) {
-		      $this->result['module'] = $seg[0];
-			  $this->result['controller'] = $seg[0] . '_' . $seg[1];
-			  $this->result['argv'] = array_slice($seg, 2);
-		  } else {
-		      $this->result['controller'] = 'index_' . $seg[0];
-		      $this->result['argv'] = array_slice($seg, 1);
-		  }
-		break;
-		}
+              $this->result['controller'] = 'index_' . $seg[0];
+              $this->result['argv'][] = $seg[1];
+          }
+        break;
+        default:
+          if (in_array($seg[0], $module)) {
+              $this->result['module'] = $seg[0];
+              $this->result['controller'] = $seg[0] . '_' . $seg[1];
+              $this->result['argv'] = array_slice($seg, 2);
+          } else {
+              $this->result['controller'] = 'index_' . $seg[0];
+              $this->result['argv'] = array_slice($seg, 1);
+          }
+        break;
+        }
       }
     }
 
-	/* Launch application controllers */
-	private function launch()
-	{
-	  $launched = false;
+    /* Launch application controllers */
+    private function launch()
+    {
+      $launched = false;
 
-	  extract($this->result);
-	  $path = A_PREFIX . "module/$module/controller/$controller.php";    
+      extract($this->result);
+      $path = A_PREFIX . "module/$module/controller/$controller.php";   
 
       if (file_exists($path)) {
-	    require 'Aeolus.php';
+        require 'Aeolus.php';
 
-	    /* Setup environment variable */
-	    global $thisGroup;
-	    $thisGroup = $module;
+        /* Setup environment variable */
+        global $thisGroup;
+        $thisGroup = $module;
 
-		/* Load controller */
+        /* Load controller */
         // TODO: use output buffer?
-	    require($path);
+        require($path);
 
-	    if (function_exists($controller)) {
-		  $launched = true;
+        if (function_exists($controller)) {
+          $launched = true;
           $controller($this->result['argv']);
-		}
-	  }
+        }
+      }
 
-	  if (!$launched)
-	    (APP_DEBUG) ? $this->debug() : $this->to_home();
-	}
+      if (!$launched)
+        (APP_DEBUG) ? $this->debug() : $this->to_home();
+    }
 
-	private function debug()
-	{
-	  extract($this->result);
-	  if ('index' == $module) {
+    private function debug()
+    {
+      extract($this->result);
+      if ('index' == $module) {
           $origin = substr($controller, strpos($controller, '_') + 1);
-          echo "Fatal: '$origin' is not a valid module name<br/>";
-          echo "Fatal: controller '$controller' not found in 'index' module<br/>";
-          echo "Tips:<br/>";
+          echo "<span style=\"color:red;\">Fatal: </span>'$origin' is not a valid module name<br/>";
+          echo "<span style=\"color:red;\">Fatal: </span>controller '$controller' not found in 'index' module<br/>";
+          echo '<div style="color:green;">Tips:</div>';
           echo "to resolve this fatal error, you could add a '$controller' controller to 'index' module, or<br/>";
           echo "add '$origin' as a module.";
-	  } else {
-	      echo "Fatal: controller '$controller' not found in '$module' module.";
-	  }
-	} 
+      } else {
+          echo "<span style=\"color:red;\">Fatal: </span>controller '$controller' not found in '$module' module.";
+      }
+    }
 
-	private function to_home()
-	{
-	  header('Location: ' . SUB_DIR);
-	  exit(0);
-	}
+    private function to_home()
+    {
+      header('Location: ' . SUB_DIR);
+      exit(0);
+    }
   }
 ?>
