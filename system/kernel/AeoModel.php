8b8a22cca6fe9e47cfb43d1cdd6dc50c3020a11e
Kinch Zhang <kinch.zhang@gmail.com>
2009-03-08 20:05:18 +0800

    fix a bug in AeoModel: getDriver() is a static method,     so we can't use '$this'.

--- system/kernel/AeoModel.php
+++ system/kernel/AeoModel.php
@@ -1,89 +1,88 @@
 <?php
-  /*
-   * Model class
-   */
 
-  class AeoModel
-  {
-    private static $driver = null;
+    /*
+     * Model class
+     */
 
-	private $data = array();
-	
-	function __construct()
-	{
-	}
-
-	public final function insert($sql)
-	{
-      $driver = self::getDriver();
-
-	  if ($driver->query($sql)) {
-	    $this->data['affected'] = mysql_affected_rows();
-	    $this->data['lastInsertId'] = mysql_insert_id();
-	  }
-
-	  return $this->data;
-	}
-	
-	public final function update($sql)
-	{
-      $driver = self::getDriver();
-
-	  if ($driver->query($sql))
-	    $this->data['affected'] = mysql_affected_rows();
-
-	  return $this->data;
-	}
-	
-	public final function delete($sql)
-	{
-      $driver = self::getDriver();
-
-	  if ($driver->query($sql))
-	    $this->data['affected'] = mysql_affected_rows();
-	  
-	  return $this->data;
-	}
-	
-	public final function select($sql)
-	{			
-      $driver = self::getDriver();
-
-	  if ($result = $driver->query($sql)) {	        
-	    $this->data['affected'] = mysql_num_rows($result);
-	    if ($this->data['affected'] > 0) {
-		  /* Fetch data */
-	      while ($dataset = mysql_fetch_assoc($result))
-	        $this->data['set'][] = $dataset;
-
-		  $dataset = null;
-	    }
-	  }
-
-	  return $this->data;
-	}
-	
-	public final function escape($v)
-	{
-	  return mysql_real_escape_string($v,self::$driver->get_link());
-	}
-
-    private static final function getDriver()
+    class AeoModel
     {
-	  if (null == self::$driver) {
-		require A_PREFIX.'config/system/database/driver.php';
+        private static $driver = null;
 
-        Aeolus::loadClass("database/$driver");
-	    self::$driver = new $driver();
-	    
-		$this->data = array(
-		  'affected' => 0,
-		  'lastInsertId' => 0,
-		  'set' => array()
-		);
-	  }
+        private $data = array(
+            'affected' => 0,
+            'lastInsertId' => 0,
+            'set' => array()
+        );
 
-      return self::$driver;
+        function __construct()
+        {
+        }
+
+        public final function insert($sql)
+        {
+            $driver = self::getDriver();
+
+            if ($driver->query($sql)) {
+                $this->data['affected'] = mysql_affected_rows();
+                $this->data['lastInsertId'] = mysql_insert_id();
+            }
+
+            return $this->data;
+        }
+
+        public final function update($sql)
+        {
+            $driver = self::getDriver();
+
+            if ($driver->query($sql))
+                $this->data['affected'] = mysql_affected_rows();
+
+            return $this->data;
+        }
+
+        public final function delete($sql)
+        {
+            $driver = self::getDriver();
+
+            if ($driver->query($sql))
+                $this->data['affected'] = mysql_affected_rows();
+
+            return $this->data;
+        }
+
+        public final function select($sql)
+        {
+            $driver = self::getDriver();
+
+            if ($result = $driver->query($sql)) {
+                $this->data['affected'] = mysql_num_rows($result);
+                if ($this->data['affected'] > 0) {
+                    /* Fetch data */
+                    while ($dataset = mysql_fetch_assoc($result))
+                    $this->data['set'][] = $dataset;
+
+                    $dataset = null;
+                }
+            }
+
+            return $this->data;
+        }
+
+        public final function escape($v)
+        {
+            return mysql_real_escape_string($v,self::$driver->get_link());
+        }
+
+        private static final function getDriver()
+        {
+            if (null == self::$driver) {
+                require A_PREFIX.'config/system/database/driver.php';
+
+                Aeolus::loadClass("database/$driver");
+                self::$driver = new $driver();
+            }
+
+            return self::$driver;
+        }
     }
-  }
 ?>
