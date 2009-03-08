998735f1d82e6fec7a228c4ae12d6b1eafe5b228
Kinch Zhang <kinch.zhang@gmail.com>
2009-03-08 15:39:31 +0800

    account module init

--- config/system/module.php
+++ config/system/module.php
@@ -1,6 +1,6 @@
 <?php
 
     # Valid modules
-
     $module[] = 'index';
+    $module[] = 'account';
 ?>
---
+++ module/account/controller/account_init.php
@@ -0,0 +1,8 @@
+<?php
+
+    /* init controller in account module */
+    function account_init()
+    {
+        echo 'Hello,world!&nbsp;(From init controller in account module)';
+    }
+?>
